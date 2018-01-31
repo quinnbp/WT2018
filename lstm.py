e844d49404b3ba29ea89509d9333131259ba9770
svouse <svouse@gmail.com>
2018-01-31 15:26:35 -0500

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -30,117 +30,113 @@ n_words = 0
 MAX_LABEL = 15
 WORDS_FEATURE = 'words'  # Name of the input words feature.
 
+class LSTM:
+  def estimator_spec_for_softmax_classification(
+      self, logits, labels, mode):
+    """Returns EstimatorSpec instance for softmax classification."""
+    predicted_classes = tf.argmax(logits, 1)
+    if mode == tf.estimator.ModeKeys.PREDICT:
+      return tf.estimator.EstimatorSpec(
+          mode=mode,
+          predictions={
+              'class': predicted_classes,
+              'prob': tf.nn.softmax(logits)
+          })
 
-def estimator_spec_for_softmax_classification(
-    logits, labels, mode):
-  """Returns EstimatorSpec instance for softmax classification."""
-  predicted_classes = tf.argmax(logits, 1)
-  if mode == tf.estimator.ModeKeys.PREDICT:
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+      optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+      train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+      return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+
+    eval_metric_ops = {
+        'accuracy': tf.metrics.accuracy(
+            labels=labels, predictions=predicted_classes)
+    }
     return tf.estimator.EstimatorSpec(
-        mode=mode,
-        predictions={
-            'class': predicted_classes,
-            'prob': tf.nn.softmax(logits)
-        })
-
-  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
-    train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
-
-  eval_metric_ops = {
-      'accuracy': tf.metrics.accuracy(
-          labels=labels, predictions=predicted_classes)
-  }
-  return tf.estimator.EstimatorSpec(
-      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
-def rnn_model(features, labels, mode):
-  # Convert indexes of words into embeddings.
-  # Creates embeddings matrix of [n_words, EMBEDDING_SIZE] and then
-  # maps word indexes of the sequence into [batch_size, sequence_length,
-  # EMBEDDING_SIZE].
-  word_vectors = tf.contrib.layers.embed_sequence(
-      features[WORDS_FEATURE], vocab_size=n_words, embed_dim=EMBEDDING_SIZE)
+  def rnn_model(self, features, labels, mode):
+    # Convert indexes of words into embeddings.
+    # Creates embeddings matrix of [n_words, EMBEDDING_SIZE] and then
+    # maps word indexes of the sequence into [batch_size, sequence_length,
+    # EMBEDDING_SIZE].
+    word_vectors = tf.contrib.layers.embed_sequence(
+        features[WORDS_FEATURE], vocab_size=n_words, embed_dim=EMBEDDING_SIZE)
 
-  # Split into list of embedding per word, while removing doc length dim.
-  # word_list results to be a list of tensors [batch_size, EMBEDDING_SIZE].
-  word_list = tf.unstack(word_vectors, axis=1)
+    # Split into list of embedding per word, while removing doc length dim.
+    # word_list results to be a list of tensors [batch_size, EMBEDDING_SIZE].
+    word_list = tf.unstack(word_vectors, axis=1)
 
-  # Create a Gated Recurrent Unit cell with hidden size of EMBEDDING_SIZE.
-  cell = tf.nn.rnn_cell.GRUCell(EMBEDDING_SIZE)
+    # Create a Gated Recurrent Unit cell with hidden size of EMBEDDING_SIZE.
+    cell = tf.nn.rnn_cell.GRUCell(EMBEDDING_SIZE)
 
-  # Create an unrolled Recurrent Neural Networks to length of
-  # MAX_DOCUMENT_LENGTH and passes word_list as inputs for each unit.
-  _, encoding = tf.nn.static_rnn(cell, word_list, dtype=tf.float32)
+    # Create an unrolled Recurrent Neural Networks to length of
+    # MAX_DOCUMENT_LENGTH and passes word_list as inputs for each unit.
+    _, encoding = tf.nn.static_rnn(cell, word_list, dtype=tf.float32)
 
-  # Given encoding of RNN, take encoding of last step (e.g hidden size of the
-  # neural network of last step) and pass it as features for softmax
-  # classification over output classes.
-  logits = tf.layers.dense(encoding, MAX_LABEL, activation=None)
-  return estimator_spec_for_softmax_classification(
-      logits=logits, labels=labels, mode=mode)
+    # Given encoding of RNN, take encoding of last step (e.g hidden size of the
+    # neural network of last step) and pass it as features for softmax
+    # classification over output classes.
+    logits = tf.layers.dense(encoding, MAX_LABEL, activation=None)
+    return estimator_spec_for_softmax_classification(
+        logits=logits, labels=labels, mode=mode)
 
+  # @param prepared training instances, labels, testing intances, labels
+  def train(train_data, train_labels):  
+    global n_words
+    tf.logging.set_verbosity(tf.logging.INFO)
 
-def main(unused_argv):
-  global n_words
-  tf.logging.set_verbosity(tf.logging.INFO)
+    # Define training and testing data
+    x_train = train_data
+    y_train = train_labels
+    x_test = test_data
+    y_test = test_labels
 
-  # Prepare training and testing data
-  x_train = #use pandas.read_excel 
-  y_train = #use pandas.read_excel 
-  x_test = #use pandas.read_excel 
-  y_test = #use pandas.read_excel 
+    # Process the vocabulary.
+    vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(
+        MAX_DOCUMENT_LENGTH)
 
-  # Process the vocabulary.
-  vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(
-      MAX_DOCUMENT_LENGTH)
+    x_transform_train = vocab_processor.fit_transform(x_train)
+    x_transform_test = vocab_processor.transform(x_test)
 
-  x_transform_train = vocab_processor.fit_transform(x_train)
-  x_transform_test = vocab_processor.transform(x_test)
+    x_train = np.array(list(x_transform_train))
+    x_test = np.array(list(x_transform_test))
 
-  x_train = np.array(list(x_transform_train))
-  x_test = np.array(list(x_transform_test))
+    n_words = len(vocab_processor.vocabulary_)
+    print('Total words: %d' % n_words)
 
-  n_words = len(vocab_processor.vocabulary_)
-  print('Total words: %d' % n_words)
+    # Build the model.
+    model_fn = rnn_model
+    classifier = tf.estimator.Estimator(model_fn=model_fn)
 
-  # Build the model.
-  model_fn = rnn_model
-  classifier = tf.estimator.Estimator(model_fn=model_fn)
+    # Train the model.
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={WORDS_FEATURE: x_train},
+        y=y_train,
+        batch_size=len(x_train),
+        num_epochs=None,
+        shuffle=True)
+    classifier.train(input_fn=train_input_fn, steps=100)
 
-  # Train the model.
-  train_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={WORDS_FEATURE: x_train},
-      y=y_train,
-      batch_size=len(x_train),
-      num_epochs=None,
-      shuffle=True)
-  classifier.train(input_fn=train_input_fn, steps=100)
-
-  # Save the model.
+    # Save the model.
     saved = tf.train.Saver()
-    
-  # Predict.
-  test_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={WORDS_FEATURE: x_test},
-      y=y_test,
-      num_epochs=1,
-      shuffle=False)
-  predictions = classifier.predict(input_fn=test_input_fn)
-  y_predicted = np.array(list(p['class'] for p in predictions))
-  y_predicted = y_predicted.reshape(np.array(y_test).shape)
+      
+    # Predict.
+    test_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={WORDS_FEATURE: x_test},
+        y=y_test,
+        num_epochs=1,
+        shuffle=False)
+    predictions = classifier.predict(input_fn=test_input_fn)
+    y_predicted = np.array(list(p['class'] for p in predictions))
+    y_predicted = y_predicted.reshape(np.array(y_test).shape)
 
-  # Score with sklearn.
-  score = metrics.accuracy_score(y_test, y_predicted)
-  print('Accuracy (sklearn): {0:f}'.format(score))
+    # Score with sklearn.
+    score = metrics.accuracy_score(y_test, y_predicted)
+    print('Accuracy (sklearn): {0:f}'.format(score))
 
-  # Score with tensorflow.
-  scores = classifier.evaluate(input_fn=test_input_fn)
-  print('Accuracy (tensorflow): {0:f}'.format(scores['accuracy']))
-
-
-if __name__ == '__main__':
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    # Score with tensorflow.
+    scores = classifier.evaluate(input_fn=test_input_fn)
+    print('Accuracy (tensorflow): {0:f}'.format(scores['accuracy']))
