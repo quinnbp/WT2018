1825aa879efbd0c88f02c80f4c2d4415ad11fa2a
svouse <svouse@gmail.com>
2018-04-02 20:39:46 -0400

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -62,7 +62,6 @@ class LSTM:
     return tf.estimator.EstimatorSpec(
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-
   def rnn_model(self, features, labels, mode):
     # Convert indexes of words into embeddings.
     # Creates embeddings matrix of [n_words, EMBEDDING_SIZE] and then
@@ -75,16 +74,11 @@ class LSTM:
     # word_list results to be a list of tensors [batch_size, EMBEDDING_SIZE].
     word_list = tf.unstack(word_vectors, axis=1)
 
-    # Create a Gated Recurrent Unit cell with hidden size of EMBEDDING_SIZE.
+    
     cell = tf.nn.rnn_cell.GRUCell(EMBEDDING_SIZE)
-
-    # Create an unrolled Recurrent Neural Networks to length of
-    # MAX_DOCUMENT_LENGTH and passes word_list as inputs for each unit.
+	
     _, encoding = tf.nn.static_rnn(cell, word_list, dtype=tf.float32)
 
-    # Given encoding of RNN, take encoding of last step (e.g hidden size of the
-    # neural network of last step) and pass it as features for softmax
-    # classification over output classes.
     logits = tf.layers.dense(encoding, MAX_LABEL, activation=None)
     return estimator_spec_for_softmax_classification(
         logits=logits, labels=labels, mode=mode)
