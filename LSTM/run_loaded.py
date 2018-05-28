8d6968e6548d7ce529bc0c0cc77b0e52d00599de
svouse <svouse@gmail.com>
2018-05-28 14:55:44 -0700

    Update run_loaded.py

--- LSTM/run_loaded.py
+++ LSTM/run_loaded.py
@@ -1,30 +1,32 @@
-from LSTM.lib_model.char_lstm import *
-
 #   TO RUN LOADED LSTM
 def runLSTM(sentences):
     #sentences= []
     #for i in range(1000):
     #    sentences.append("this is a fucking offensive sentence goddamnit")
     print('Creating LSTM from load')
-    # if sentences is not None:
-    #     for value in sentences:
-    #         print('processing sentence: %s' % value)
+
+    from lib_model.char_lstm import *
 
     network = LSTM()
     network.build()
     preds =[]
     feeder = []
-    i = 0
-    sentences = [t.getFullTweet() for t in sentences]
-
+    i =0
     if sentences is not None:
         for sentence in sentences:
             feeder.append(sentence)
-            if i % 1000 == 0:
-                preds.append(network.predict_sentences(feeder))
+            if i % 100 == 0 or i % len(sentences) == 0:
+                network.predict_sentences(feeder)
+                preds.append(network.predlist)
                 feeder = []
-                print(preds)
-            i += 1
+            i+=1;
+    return network.predlist
 
-    return preds
+#TEST
+#if __name__ == '__main__':
+    #sentence = "test"
+    #l = []
+    #for i in range(100):
+    #    l.append(sentence)
+    #runLSTM(l)
 
